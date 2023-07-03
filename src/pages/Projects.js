9ddadc5fb652edce8121f23ea9dc2075571ef2cb
Lea Cassegrain <lea.csgn@gmail.com>
2023-07-03 02:17:30 +0200

    modifications

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -9,7 +9,7 @@ const Projects = () => {
       image: require("../assets/64a17f5ff8f8e65589ff44dd--lea2119-booking-app.netlify.app_.png"),
       title: "BOOKING CLONE APP",
       tags: ["REACT", "MONGO", "EXPRESS", "NODE"],
-      href: "https://64a194d8f8f8e6613cff44cc--deluxe-strudel-c69959.netlify.app/",
+      href: "https://lea2119-booking.netlify.app/",
       git: "https://github.com/Lea2119/booking",
       description:
         "This project is a booking website clone that allows users to create an account, log in, and update their profiles. Users can search for hotels based on location and dates, view hotel details, and book rooms.",
